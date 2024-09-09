b9f716cc76a22ac7e801d9410c3494ef6a76df7a
21120447 <haonhat2729@gmail.com>
2024-09-09 22:48:46 +0700

    chore: limit input

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import { Textarea } from "@/components/ui/textarea";
 import SelectVoice from "@/components/ui/select/select-voice";
 import PlayAudio from "@/components/button/play-audio";
 
+const maxLength = 20;
+
 export default function Home() {
   const [text, setText] = React.useState("");
   const [voiceId, setVoiceId] = React.useState("Xb7hH8MSUJpSbSDYk0k2");
+  const [showWarning, setShowWarning] = React.useState(false);
 
   return (
     <div className="container max-w-2xl mx-auto py-3 px-2">
@@ -20,8 +23,17 @@ export default function Home() {
             <PlayAudio text={text} voiceId={voiceId} />
         </div>
 
-        <Textarea placeholder="Enter a prompt text to speech" onChange={(e) => setText(e.target.value)} value={text} className="h-[200px] text-base" />
+        <Textarea placeholder="Enter a prompt text to speech" onChange={(e) => {
+          setText(e.target.value);
+          if (e.target.value.length >= maxLength) setShowWarning(true);
+          else setShowWarning(false);
+        }} value={text} className="h-[200px] text-base" maxLength={maxLength} />
       </div>
+      {showWarning &&
+        <div className="text-sm text-blue-500 italic">
+          Do xài free nên giới hạn{" "}{maxLength}{" "}kí tự =))
+        </div>
+      }
     </div>
   );
 }
